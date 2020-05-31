e17e819c5e3cd4825fe597f768eba087fd7e8286
Asael Martínez <asael.mb@hotmail.com>
2020-05-30 20:57:34 -0600

    Se terminó la animación y comportamiento del enemigo koopa

--- graficas/script.js
+++ graficas/script.js
@@ -2,34 +2,35 @@ var panel = document.getElementById("plano")
 var position = panel.getBoundingClientRect()
 const x = panel.clientWidth
 const y = panel.clientHeight 
-var node,node2,node3
+var node
 
 console.log(x,y)
 
 function main(){    
     node = document.createElement("div")
     node.className = "point"     
-    panel.appendChild(node)
-    node2 = document.createElement("div")
-    node2.className = "point"     
-    //panel.appendChild(node2)
+    panel.appendChild(node)    
 }
-var FPS = 100
-setInterval(function(){
-    move(node)
-    //move2(node2)
-},1000/FPS)
 
 var cont=0
-var cont2=0
 function move(nodo){    
     if(cont ==  0) cont = panel.clientWidth - 20
     nodo.style.top = `${100*(Math.cos(cont/50))+(y/2)}px`
     nodo.style.left = `${cont--}px`
 }
 
-function move2(nodo){    
-    if(cont2 ==  0) cont2 = panel.clientWidth - 20
-    nodo.style.top = `${-100*Math.cos(cont2/20)+(y/2)}px`
-    nodo.style.left = `${cont2--}px`
+var nImage=1
+function charMove(){
+    node.style.backgroundImage = `url('../Koopa/${nImage}.png')`
+    nImage++
+    if(nImage == 10) nImage = 1
 }
+
+var FPS = 100
+setInterval(function(){
+    move(node)    
+},1000/FPS)
+
+setInterval(function(){
+    charMove()
+},100)
